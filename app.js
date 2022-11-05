d88de18f853c896d4e672b95e86e1b8d102e7ad8
YuliaChen8179 <spirit6135@gmail.com>
2022-11-05 22:28:00 +0800

    Initial Project

---
+++ app.js
@@ -0,0 +1,41 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public'))
+
+// routes setting
+//render index
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+//render show
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.filter(restaurant => restaurant.id == req.params.restaurant_id)
+  res.render('show', { restaurants: restaurant[0] })
+})
+
+//render search
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurantFilted = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurantFilted, keyword: keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})

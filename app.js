546f62eb5b9ba49745d82fc2bde10c6ed621a2dc
YuliaChen8179 <spirit6135@gmail.com>
2022-11-17 16:11:31 +0800

    connect mongoose DB

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ const port = 3000
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+
+//require mongoose
+const mongoose = require('mongoose')
+
+//Mongoose connect
+require('dotenv').config()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))

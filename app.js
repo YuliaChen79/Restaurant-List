339e2dced292273019214d295a916a711cb628d0
YuliaChen8179 <spirit6135@gmail.com>
2022-11-20 14:23:27 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -4,27 +4,10 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 
-//require mongoose
-const mongoose = require('mongoose')
-
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-const Restaurant = require('./models/restaurant')
-//Mongoose connect
-require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -35,8 +18,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-
 
 // start and listen on the Express server
 app.listen(port, () => {
---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+//require mongoose
+const mongoose = require('mongoose')
+
+//Mongoose connect
+require('dotenv').config()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db

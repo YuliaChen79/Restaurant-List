c0ce68ce7ed08589a2fa06ceca0cd753acbcf1f9
YuliaChen8179 <spirit6135@gmail.com>
2022-11-20 14:16:33 +0800

    refactor: modify express routes

---
+++ routes/modules/home.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../../models/restaurant')
+
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+//render search
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      const restaurantFilted = restaurants.filter(data => {
+        return data.name.toLowerCase().includes(keyword.toLowerCase()) || data.category.toLowerCase().includes(keyword.toLowerCase())
+      })
+      res.render('index', { restaurants: restaurantFilted, keyword: keyword })
+    })
+    .catch(error => console.log(error))
+})
+
+module.exports = router

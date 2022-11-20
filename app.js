9a72e265154472902a6df9ec706f001597b2deda
YuliaChen8179 <spirit6135@gmail.com>
2022-11-20 19:41:28 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -4,26 +4,10 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 
-//require mongoose
-const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
-const methodOverride = require('method-override') 
-
-//Mongoose connect
-require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const methodOverride = require('method-override')
+const routes = require('./routes')
+require('./config/mongoose')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -33,89 +17,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-// routes setting
-//render index
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.error(error))
-})
-
-//render search
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      const restaurantFilted = restaurants.filter(data => {
-        return data.name.toLowerCase().includes(keyword.toLowerCase()) || data.category.toLowerCase().includes(keyword.toLowerCase())
-      })
-      res.render('index', { restaurants: restaurantFilted, keyword: keyword })
-    })
-    .catch(error => console.log(error))
-})
-
-//render new
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-
-//add new restaurant
-app.post('/restaurants', (req, res) => {
-  Restaurant.create(req.body)
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-//render show
-app.get("/restaurants/:restaurantId", (req, res) => {
-  const restaurantId = req.params.restaurantId
-  Restaurant.findById(restaurantId)
-    .lean()
-    .then(restaurantData => res.render("show", { restaurantData }))
-    .catch(err => console.log(err))
-})
-
-//render edit
-app.get('/restaurants/:restaurantId/edit', (req, res) => {
-  const restaurantId = req.params.restaurantId
-  Restaurant.findById(restaurantId)
-    .lean()
-    .then(restaurantData => res.render("edit", { restaurantData }))
-    .catch(err => console.log(err))
-})
-
-//edit resstaurant data
-app.put('/restaurants/:restaurantId', (req, res) => {
-  const restaurantId = req.params.restaurantId
-  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  Restaurant.findById(restaurantId)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${restaurantId}`))
-    .catch(error => console.log(error))
-})
-
-//delete restaurant
-app.delete('/restaurants/:restaurantId', (req, res) => {
-  const restaurantId = req.params.restaurantId
-  Restaurant.findById(restaurantId)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 // start and listen on the Express server
 app.listen(port, () => {

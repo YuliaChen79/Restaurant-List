e34baf87f4ff140155410b673a7b03577b537c8c
YuliaChen79 <spirit6135@gmail.com>
2023-02-10 02:01:06 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,9 +1,48 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const restaurantList = require('../restaurant.json').results
 const db = require('../../config/mongoose')
+const User = require('../user')
+const SEED_USER = [
+  {
+    name: 'userA',
+    email: 'user1@example.com',
+    password: '12345678',
+    index: [0, 1, 2]
+  },
+  {
+    name: 'userB',
+    email: 'user2@example.com',
+    password: '12345678',
+    index: [3, 4, 5]
+  }
+]
 
-// 連線成功
 db.once('open', () => {
-  Restaurant.create(restaurantList)
-  console.log('done')
+  return Promise.all(SEED_USER.map(user => {
+    const { name, email, password, index } = user
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name: name,
+        email: email,
+        password: hash
+      }))
+      .then( user => {
+        const restaurants = index.map( index => {
+          const restaurant = restaurantList[index]
+          restaurant.userId = user._id
+          return restaurant
+        })
+        return Restaurant.create(restaurants)
+      })
+  }))
+  .then(() => {
+    console.log('done')
+  })
 })
